85cabf5bc46b7021e120f93100decadf0972eb5d
Harshit sharma <programming966051@gmail.com>
2024-12-29 23:42:44 +0530

    add image container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Hero from './component/Hero'
 
 function App() {
  
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Navbar />
+    <Hero />
 
     </>
   )
---
+++ src/component/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/css/heroo.scss";
+const Hero = () => {
+    return (
+        <div className="main-div">
+            <div className="content-container">
+<h1>hello i am harshit srama </h1>
+            </div>
+            <div className="image-container">
+<img src="../assets/images/shoe_image.png" alt="" />
+            </div>
+        </div>
+    );
+};
+export default Hero;
